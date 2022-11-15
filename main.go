9bd5eaadacde842a7f3d583746123742ee1a52cb
sangeui <sangeuis@icloud.com>
2022-11-16 00:22:31 +0900

    new recipe endpoint

--- main.go
+++ main.go
@@ -1,12 +1,25 @@
 package main
 
+/*
+GET		/recipes				Returns a list of recipes
+POST	/recipes				Creates a new recipe
+PUT		/recipes/{id} 			Updates an existing recipe
+DELETE	/recipes/{id}			Deletes an existing recipe
+GET		/recipes/search?tag=X	Searches for recipes by tags
+*/
+
 import (
+	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/rs/xid"
 )
 
+var recipes []Recipe
+
 type Recipe struct {
+	ID           string    `json:"id"`
 	Name         string    `json:"name"`
 	Tags         []string  `json:"tags"`
 	Ingredients  []string  `json:"ingredients"`
@@ -14,7 +27,29 @@ type Recipe struct {
 	PublishedAt  time.Time `json:"publishedAt"`
 }
 
+func NewRecipeHandler(c *gin.Context) {
+	var recipe Recipe
+
+	if err := c.ShouldBindJSON(&recipe); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error()})
+
+		return
+	}
+
+	recipe.ID = xid.New().String()
+	recipe.PublishedAt = time.Now()
+	recipes = append(recipes, recipe)
+
+	c.JSON(http.StatusOK, recipe)
+}
+
+func init() {
+	recipes = make([]Recipe, 0)
+}
+
 func main() {
 	router := gin.Default()
+	router.POST("/recipes", NewRecipeHandler)
 	router.Run()
 }
